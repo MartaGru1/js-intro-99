0ad36a4f3d5af7c1a9295733dccbbf86d548a906
MartaGru1 <marta.hrusht0211@gmail.com>
2024-02-24 12:35:44 -0600

    added pop method

--- array-methods.js
+++ array-methods.js
@@ -3,4 +3,10 @@ const arr = ['Mouse', 'Laptop'];
 // push() method is used to add an element to the tail-end of the array
 arr.push('Phone');
 
-console.log(arr); // [ 'Mouse', 'Laptop', 'Phone' ]
+console.log(arr); // [ 'Mouse', 'Laptop', 'Phone' ] 
+
+
+// pop() method is used toy remove the last element
+arr.pop();
+
+console.log(arr); // [ 'Mouse', 'Laptop' ]
